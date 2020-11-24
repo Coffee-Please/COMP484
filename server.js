c936caf7909325cd666767905c912ad0efb21ebd
Coffee-Please <priya.singh.310@my.csun.edu>
2020-11-24 11:47:56 -0800

    2516 Chat messages are sent and recieved on both ends

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 // REQUIRED PACKAGES
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
+// VARIABLES
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // SET STATIC FOLDER
 app.use(express.static(path.join(__dirname, 'public')));
 
+// When client connects, run socket.io
+io.on('connection', socket => {
+    // This runs when the client connects
+    socket.emit('message', 'Welcome to the Chatroom');
+
+    // Broadcast to all when a client connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    // This runs when the client disconnects
+    socket.on('disconnect', () => {
+         io.emit('message', 'A user has left the chat');
+});
+
+    // Listen for chat Message
+    socket.on('chatMessage', (msg) => {
+    io.emit('message', msg);
+});
+});
+
+// SET PORT TO SEND DATA TO
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
