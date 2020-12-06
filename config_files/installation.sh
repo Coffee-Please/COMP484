b42b28693ec35f5fec5577ef7e2a2a114ff71141
Coffee-Please <priya.singh.310@my.csun.edu>
2020-12-05 21:36:26 -0800

    Testing changes

--- config_files/installation.sh
+++ config_files/installation.sh
@@ -6,14 +6,18 @@
 #
 #----------------------------------------------
 # NPM INSTALLATION
-#----------------------------------------------
+#---------------------------------------------
+# npm - node package manager for NodeJS. Puts modules
+# where NodeJS can find them
 sudo apt install npm
 #
 #----------------------------------------------
 # NODEJS 14.X INSTALLATION
 #----------------------------------------------
+# software-properties-common - scripts for easy managing of PPAs
+# nodejs - open source server environment
 sudo apt-get install software-properties-common
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - # gets pakage of version 14.x of nodejs from website
 sudo apt-get install nodejs
 #
 #----------------------------------------------
@@ -23,9 +27,13 @@ node --version
 npm --version
 #
 #----------------------------------------------
-# INSTALL SOFTWARE
+# INSTALL SOFTWARE DEPENDENCIES
 #----------------------------------------------
+# express - web framework
+# socket.io - web socket framework
+# moment.js - date/time formatter
 npm install express socket.io moment
+# nodemon - automatically restarts the server when changes are made
 npm install -D nodemon
 #
 # END INSTALLATION.SH
