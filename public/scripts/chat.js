363e58ea3a37a0976a74dd615fe3f3ed97f18cdd
Coffee-Please <priya.singh.310@my.csun.edu>
2020-12-14 19:03:39 -0800

    Cleaning up files - Commenting and cutting unecessary code

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -6,86 +6,88 @@
 //======================================================================
 
 // Variables
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
+const socket = io(); // Use socket functions
 
 // Get username and room from URL
 const {username, room} = Qs.parse(location.search, {ignoreQueryPrefix: true});  // Ignore special characters in beginnng of query string target
-const socket = io(); // Use socket functions
 
 
 // Join Chat room
-socket.emit('joinRoom', {
-    username,
-    room
-}); // end socket
+socket.emit('joinRoom', {username, room}); // end socket
 
 
 // Get room and users
 socket.on('roomUsers', ({room, users}) => {
-    outputRoomName(room);
-    outputUsers(users);
+
+	outputRoomName(room);
+
+	outputUsers(users);
 }); // end socket
 
 
 // Message from server
 socket.on('message', message => {
+	// Convert UTC time to client time
+	message.time = new Date(message.time).toLocaleTimeString([], {timeStyle: "short"});
 
-    // Convert UTC time to client time
-    message.time = new Date(message.time).toLocaleTimeString([], {timeStyle: "short"});
-    outputMessage(message);
+	outputMessage(message);
 
-   // Scroll down when a message is sent by user
-    chatMessages.scrollTop = chatMessages.scrollHeight;
+	// Scroll down when a message is sent by user
+	 document.querySelector('.chat-messages').scrollTop =  document.querySelector('.chat-messages').scrollHeight;
 }); // end socket
 
 
 // Message submit handler, Listens when the user clicks submit
-chatForm.addEventListener('submit', (e) => {
-    e.preventDefault(); // prevent submission to file (default behavior)
+document.getElementById('chat-form').addEventListener('submit', (e) => {
+	// prevent submission to file (default behavior)
+	e.preventDefault();
 
-    // Store inputted message
-    const msg = e.target.elements.msg.value;
+	// Store inputted message
+	const msg = e.target.elements.msg.value;
 
-    // Emit message to the server
-    socket.emit('chatMessage', msg);
+	// Emit message to the server
+	socket.emit('chatMessage', msg);
 
-    // Clear input box after message is sent
-    e.target.elements.msg.value = '';
+	// Clear input box after message is sent
+	e.target.elements.msg.value = '';
 
-    // Focus cursor in input box
-    e.target.elements.msg.focus();
-}); // end chatForm
+	// Focus cursor in input box
+	e.target.elements.msg.focus();
+}); // end addEventListener
 
 
 // Output message to DOM
 function outputMessage(message) {
 	// Create div in HTML to hold message
-    const div = document.createElement('div');
-    div.classList.add('message');
-    div.innerHTML = `<p class="meta">${message.username}
-	<span>${message.time}</span>
-	</p>
-	<p class="text">${message.text}</p>`;
-    document.querySelector('.chat-messages').appendChild(div);
+	const div = document.createElement('div');
+
+	div.classList.add('message');
+
+	div.innerHTML = `<p class="meta">${message.username}
+		<span>${message.time}</span>
+		</p>
+		<p class="text">${message.text}</p>`;
+
+	document.querySelector('.chat-messages').appendChild(div);
 } // end outputMessages
 
 
-// Add room name to DOM
+// Adds room name to chat room
 function outputRoomName(room){
-    roomName.innerHTML = room;
+	// Add the Room name to the chat box
+	 document.getElementById('room-name').innerHTML = room;
 } // end outputRoomName
 
 
-//Add users to Users List DOM
+//Add users to Users List in sidebar
 function outputUsers(users) {
-    userList.innerHTML = `${users.map(user => `<li>${user.username}</li>`).join('')}`;
+	// Add users to the Users list
+	 document.getElementById('users').innerHTML = `${users.map(user => `<li>${user.username}</li>`).join('')}`;
 } // end outputUsers
 
 
 // Redirect user to main page after they leave the room
 function leaveRoom() {
+	// Redirect to the homepage
 	location.href = 'index.html';
 }
